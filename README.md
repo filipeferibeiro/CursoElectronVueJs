55c08c48e070facdec12c5d726300f56996188d2
Filipe Fernandes <filipeferibeiro@MacBook-Air-de-Filipe.local>
2021-08-12 23:36:13 -0300

    Adicionando o projeto

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # CursoElectronVueJs
+
+Resumo: Projeto feito no curso de Electron com VueJs da Udemy, a aplicação consiste em uma leitura de arquivos de legenda para extrair e exibir as palavras mais faladas.
+
+Tecnologias: JavaScript, VueJs e Electron.
+
+<img src="https://github.com/filipeferibeiro/CursoElectronVueJs/blob/main/screen.png">
---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <v-toolbar-title>Most Used Words</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <Home />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Home from './components/Home';
+
+export default {
+  name: 'App',
+
+  components: {
+    Home
+  },
+
+  data: () => {
+    return {
+      groupedWords: [
+        { name: 'i', amount: 1234 },
+        { name: 'you', amount: 900 },
+        { name: 'he', amount: 853 },
+      ]
+    }
+  },
+};
+</script>
